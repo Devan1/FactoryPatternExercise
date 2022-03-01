e2de724958f3b0176d1539087bdfa4dd1b2eebe1
Devan <deshlock010101@hotmai.com>
2022-03-01 10:55:14 -0600

    Add project files.

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactoryPatternExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How many tires does your vehicle have? ");
+            string userInput = Console.ReadLine(); 
+
+            VehicleFactory vehicleFactory = new VehicleFactory();
+            IVehicle firstCar = vehicleFactory.GetVehicle(userInput);
+            firstCar.Drive();
+
+            Console.WriteLine("How many tires does your vehicle have? ");
+            userInput = Console.ReadLine();
+            IVehicle firstMotorcycle = vehicleFactory.GetVehicle(userInput);
+            firstMotorcycle.Drive();
+
+
+        }
+    }
+}
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    public class VehicleFactory
+    {
+        public IVehicle GetVehicle(string userInput)
+        {
+            switch (userInput.ToLower())
+            {
+                case "4":
+                    return new Car();                   
+                case "2":
+                    return new Motorcycle();                    
+                default:
+                    return new Car();
+                    
+            }
+        }
+    }
+}
